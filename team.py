a566525a0251996f2bd6472fa28db4f7824c7ecf
sammi <txl.sammi@gmail.com>
2023-04-30 00:51:19 +1000

    added team file

---
+++ team.py
@@ -0,0 +1,30 @@
+# COMP30024 Artificial Intelligence, Semester 1 2023
+# Project Partner Nomination
+
+# Fill in your team details here. Make sure you always include this file in
+# future project submissions *unchanged* -- this allows us to keep track of who
+# is submitting what, and ensures credit is awarded correctly.
+
+metadata = {
+    # Choose a team name! Try to make it unique in some way, as it will be
+    # used to identify your team in the tournament at the end of the semester.
+    # - Allowed characters: A-Z, a-z, 0-9, -, _
+    # - Minimum length: 3 characters
+    # - Maximum length: 20 characters
+    "team_name": f"Baymax", 
+
+    # Fill in the student IDs and emails of both team members here. There must
+    # be exactly two members in the list. 
+    "team_members": [
+        {
+            "name": "Ting Xin Li",
+            "student_id": "1271851",
+            "email": "tingxin@student.unimelb.edu.au" # Must be @student.unimelb.edu.au address
+        },
+        {
+            "name": "Gonzalo Molina",
+            "student_id": "1085253",
+            "email": "gmolina@student.unimelb.edu.au" # Must be @student.unimelb.edu.au address
+        }
+    ]
+}
